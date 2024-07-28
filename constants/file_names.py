3cc0a6674f42a43257aa6447ad000348095ad9ef
Imortalco <vanko.krastev@gmail.com>
2024-07-28 22:16:09 +0300

    Adding renamed files.

---
+++ constants/file_names.py
@@ -0,0 +1,31 @@
+#Folder names
+CSV_FOLDER = 'data'
+PREDICTIONS_FOLDER = 'predictions'
+
+#Data file names
+COIN_FILE_NAME = f'{CSV_FOLDER}/coin_data'
+PREDICTIONS_FILE_NAME = f'{PREDICTIONS_FOLDER}/coin_predictions'
+
+#Data file names
+BTC_FILE_NAME = f"{CSV_FOLDER}/btc_data"
+ETH_FILE_NAME = f"{CSV_FOLDER}/eth_data"
+SOL_FILE_NAME = f"{CSV_FOLDER}/sol_data"
+XRP_FILE_NAME = f"{CSV_FOLDER}/xrp_data"
+DOGE_FILE_NAME = f"{CSV_FOLDER}/doge_data"
+CARDANO_FILE_NAME = f"{CSV_FOLDER}/cardano_data"
+SHIBA_FILE_NAME = f"{CSV_FOLDER}/shiba_data"
+POLKADOT_FILE_NAME = f"{CSV_FOLDER}/polkadot_data"
+NEAR_FILE_NAME = f"{CSV_FOLDER}/near_data"
+POLYGON_FILE_NAME = f"{CSV_FOLDER}/dot_data"
+
+#File paths list
+FILE_PATHS = [BTC_FILE_NAME, 
+              ETH_FILE_NAME, 
+              SOL_FILE_NAME, 
+              XRP_FILE_NAME, 
+              DOGE_FILE_NAME,
+              CARDANO_FILE_NAME, 
+              SHIBA_FILE_NAME, 
+              POLKADOT_FILE_NAME, 
+              NEAR_FILE_NAME, 
+              POLYGON_FILE_NAME]
---
+++ plot_predictions.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+def calculate_metrics_by_group(group, actual, predicted):
+    mae = mean_absolute_error(group[actual], group[predicted])
+    mse = mean_squared_error(group[actual], group[predicted])
+    rmse = np.sqrt(mse)
+    r2 = r2_score(group[actual], group[predicted])
+    return pd.Series({'MAE' : mae, 'MSE': mse, 'RMSE': rmse, 'R2': r2})
+
+def plot_predictions(df, actual_column, prediction_column, key_column, time_col):
+    scaler = MinMaxScaler()
+    unique_ids = df[key_column].unique()
+    df[time_col] = pd.to_datetime(df[time_col])
+    df[actual_column] = pd.to_numeric(df[actual_column], errors='coerce')
+    df[prediction_column] = pd.to_numeric(df[prediction_column], errors='coerce')
+    # df[actual_column] = scaler.fit_transform(df[[actual_column]])
+    # df[prediction_column] = scaler.fit_transform(df[[prediction_column]])
+    plt.figure(figsize=(15, 15))
+    #plt.yscale('log')
+
+    for id in unique_ids:
+        group = df[df[key_column] == id]
+        plt.plot(group[time_col], group[actual_column], label=f'Actual - {id}')
+        plt.plot(group[time_col], group[prediction_column], linestyle='--', label=f'Predicted - {id}')
+    
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.title('Actual vs Predicted values for each ID')
+    
+    plt.legend()
+    plt.show()
+
+def compare_predictions(df_actual, df_predicted, actual_column, prediction_column, key_column = 'unique_id', time_col = 'time'):
+    df_merged = pd.merge(df_actual[[key_column, actual_column, time_col]],
+                         df_predicted[[key_column, prediction_column, time_col]],
+                         on=[key_column, time_col])
+    metrics_by_group = df_merged.groupby(key_column).apply(lambda group: calculate_metrics_by_group(group,actual_column, prediction_column)).reset_index()
+    print(metrics_by_group)
+    plot_predictions(df_merged, actual_column, prediction_column, key_column, time_col)
